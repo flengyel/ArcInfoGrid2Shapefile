0de7e69d0a7c3a4fd4d9446b59a8a83300a48733
Florian Lengyel <florian.lengyel@gmail.com>
2013-04-16 13:07:33 -0400

    Handling of ArcInfo .clr files added, with scaling to match reclassified values from arcinfo.py.

--- txt2cartocss.py
+++ txt2cartocss.py
@@ -7,9 +7,12 @@
 import sys
 import argparse as arg
 
-descript = "Convert BC-Consulting QGIS Color Table plugin syntax to CartoDB.CSS"
+descript = "Convert ArcGIS or BC-Consulting QGIS Color Table plugin syntax to CartoDB.CSS"
 epistr   = "Software is released under the GPL (c) 2013 Florian Lengyel, CUNY Environmental CrossRoads Initiative, Advanced Science Research Center, The City College of New York. Contact: gmail/skype/twitter florianlengyel."
 parser  = arg.ArgumentParser( description = descript, epilog=epistr )
+parser.add_argument('--clr',
+		    help='Use ArcInfo .clr format',
+		    action='store_true')
 parser.add_argument('--ge', 
 		    help='Use [column_name >= value] instead of >',
 		    action='store_true')
@@ -24,13 +27,16 @@ parser.add_argument( '--line_opacity',
 parser.add_argument( '-c', '--column_name', 
 		     help='Name of CartoDB column',
 		     default='value' )
+parser.add_argument( '-s', '--scaling',
+		     type=int,
+		     default=1,
+		     help='Scale values by int multiple; e.g., a power of 10.')
 parser.add_argument( 'table_name', 
 		     help='Name of CartoDB table')
 parser.add_argument( 'color_table', 
 		     type=arg.FileType('r'),
 		     help='Name of BC-Consult Color Table file' )
 
-args = parser.parse_args()
 
 class CartoCSS(object):
   def __init__(self, args):
@@ -54,14 +60,23 @@ class CartoCSS(object):
     for i in range(0, self.openBraces):
       sys.stdout.write('}')
 
-# discard the first line
-args.color_table.readline()
-
+args = parser.parse_args()
 css = CartoCSS(args)
 
+# discard the first line
+# Format of BC consult includes a header line
+if not args.clr:
+  args.color_table.readline()
+
+# if ArcInfo .clr format, the separator is a space
+if args.clr:
+  sep = ' '
+else:
+  sep = ','
+
 for line in args.color_table:
-  field = line.split(',')
-  map = {  'value': field[0].strip(), 
+  field = line.split(sep)
+  map = {  'value': str(int(field[0].strip())*args.scaling), 
 	   'red'  : field[1].strip(),
 	   'green': field[2].strip(),
 	   'blue' : field[3].strip()}
